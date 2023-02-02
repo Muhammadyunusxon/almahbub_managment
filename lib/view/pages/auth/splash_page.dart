3e4dd1a60db869954652fa79bc5823979dfa22f5
Muhammadyunusxon <muhammadyunusxonabdumannopov9@gmail.com>
2023-02-02 17:02:02 +0500

    add splash

---
+++ lib/view/pages/auth/splash_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import '../../../constants.dart';
+import '../../../controller/home_controller.dart';
+import '../../../controller/local_store/local_store.dart';
+import '../../style/style.dart';
+import '../home_screen//home_page.dart';
+import 'login_page.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      String? docId = await LocalStore.getDocId();
+      if (docId == null) {
+        // ignore: use_build_context_synchronously
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (_) => const LoginPage()),
+            (route) => false);
+      } else {
+        // ignore: use_build_context_synchronously
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (_) => const HomePage()),
+            (route) => false);
+      }
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kGreenColor,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Text(
+            'AL MAHBUB',
+            style: Style.brandStyleBold(),
+          ),
+        ),
+      ),
+    );
+  }
+}
