81c6a3f55f4c5f0c0f187c7d550ce586d7d7b4b8
Muhammadyunusxon <muhammadyunusxonabdumannopov9@gmail.com>
2023-02-05 10:53:43 +0500

    add const

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const kBrandColor = Color(0xFFFBBA00);
+const kTextDarkColor = Color(0xff01000D);
+const kGreenColor = Color(0xff0F6657);
+const kYellowColor = Color(0xffFBA808);
+const kWhiteColor = Color(0xffffffff);
+const kTextGreenColor = Color(0xff00AD8F);
+const kBGColor = Color(0xffF8F9FA);
+
+const kDefaultPadding = 20.0;
+
+const kDefaultShadow = BoxShadow(
+  offset: Offset(0, 4),
+  blurRadius: 4,
+  color: Colors.black26,
+);
