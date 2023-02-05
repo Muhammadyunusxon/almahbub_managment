245035b0c2507d1c262c337189cd45386c0a4198
Muhammadyunusxon <muhammadyunusxonabdumannopov9@gmail.com>
2023-02-05 11:24:08 +0500

    add Type Dialog

---
+++ lib/view/pages/add_screen/widgets/type_dialog.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+
+import '../../../../constants.dart';
+import '../../../../controller/product_controller.dart';
+import '../../../style/style.dart';
+import 'my_form_field.dart';
+
+class TypeDialog extends StatefulWidget {
+  const TypeDialog({Key? key}) : super(key: key);
+
+  @override
+  State<TypeDialog> createState() => _TypeDialogState();
+}
+
+class _TypeDialogState extends State<TypeDialog> {
+  late TextEditingController _textEditingController;
+
+  @override
+  void initState() {
+    _textEditingController = TextEditingController();
+    super.initState();
+  }
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final state = context.watch<ProductController>();
+    final event = context.read<ProductController>();
+    return Container(
+      padding: const EdgeInsets.all(24),
+      decoration: BoxDecoration(
+          color: kGreenColor, borderRadius: BorderRadius.circular(14)),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          12.verticalSpace,
+          MyFormFiled(controller: _textEditingController, title: "O'lchov birligi"),
+          18.verticalSpace,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Container(
+                  padding:
+                  const EdgeInsets.symmetric(vertical: 8, horizontal: 18),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: kWhiteColor.withOpacity(0.5),
+                  ),
+                  child: Text(
+                    "Cencel",
+                    style: Style.textStyleSemiBold(
+                        textColor: kGreenColor, size: 14),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  event.addType(name: _textEditingController.text, onSuccess: () {
+                    Navigator.pop(context);
+                  });
+                },
+                child: Container(
+                  padding:
+                  const EdgeInsets.symmetric(vertical: 8, horizontal: 18),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: kWhiteColor.withOpacity(0.5),
+                  ),
+                  child:state.isSaveCategoryLoading? const CircularProgressIndicator(color: kGreenColor,strokeWidth: 3,): Text(
+                    "OK",
+                    style: Style.textStyleSemiBold(
+                        textColor: kGreenColor, size: 14),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
