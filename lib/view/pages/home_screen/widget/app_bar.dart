dadd91c2f62cb17d5ae20d67d9a4dd9e3e778988
Muhammadyunusxon <muhammadyunusxonabdumannopov9@gmail.com>
2023-02-06 16:15:23 +0500

    add appbar

---
+++ lib/view/pages/home_screen/widget/app_bar.dart
@@ -0,0 +1,63 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../constants.dart';
+import '../../../style/style.dart';
+import 'search_form_field.dart';
+
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      height: 180,
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(horizontal: 28, vertical: 24),
+      color: kGreenColor,
+      child: Column(
+        children: [
+          const SizedBox(height: 24),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              RichText(
+                  text: TextSpan(children: [
+                    TextSpan(
+                        text: "Al",
+                        style: Style.brandStyle(textColor: kWhiteColor)),
+                    TextSpan(
+                        text: " Mahbub",
+                        style: Style.brandStyle(textColor: kYellowColor)),
+                  ])),
+              IconButton(
+                splashRadius: 24,
+                onPressed: () {
+                  print("Navigator");
+                },
+                icon: SizedBox(
+                    height: 18,
+                    width: 18,
+                    child: Image.asset("assets/images/menu.png")),
+              )
+            ],
+          ),
+          const SizedBox(height: 10),
+          Row(
+            children: [
+              SvgPicture.asset('assets/svg/location.svg', height: 14),
+              const SizedBox(width: 5),
+              Text(
+                "Yangiyer bozori",
+                style: Style.textStyleNormal(
+                    textColor: kWhiteColor, size: 14),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
